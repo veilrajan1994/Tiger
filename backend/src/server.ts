004169c7007d002f2a638636086b882fca3f7bf7
veilrajan.M <veilrajan.m@idp.com>
2026-04-01 16:57:50 +0530

    cross origin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CORS_ORIGIN ?? 'http://localhost:3000',
+  origin: "*",
   credentials: true
 }));
 
